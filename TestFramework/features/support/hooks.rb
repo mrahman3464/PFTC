75c1994c147855e670963492083cf112156c694d
MOKLASUR RAHMAN <mrahman3464@users.noreply.github.com>
2019-01-15 20:28:23 -0500

    Update hooks.rb

--- TestFramework/features/support/hooks.rb
+++ TestFramework/features/support/hooks.rb
@@ -1,7 +1,5 @@
-#Author: MOKLASUR RAHMAN
-#Date:07/11/2016
-include SauceLabs
-include ReuseableFunction
+
+
 
 # Before do
 #
@@ -58,6 +56,7 @@ Before do
       # @browser = SauceLabs.watir_browser(ENV['BROWSER'].to_sym)
 
       if(ENV['BROWSER']== "chrome")
+          Selenium::WebDriver::Chrome.driver_path="/usr/bin/chromedriver"
         @browser = Watir::Browser.new ENV['BROWSER'].to_sym,:switches => %w[--disable-extensions  --ignore-certificate-errors --disable-popup-blocking --disable-translate]
       else
         @browser = Watir::Browser.new ENV['BROWSER'].to_sym
@@ -172,23 +171,6 @@ end
 
 
 
-# at_exit do
-#   ENV['ARCHIVE_RESULTS'] = 'no' if ENV['ARCHIVE_RESULTS'].nil?
-#   if ENV['ARCHIVE_RESULTS']=="yes"
-#     Dir.mkdir("resultsarchive") if Dir["resultsarchive"].empty?
-#     folder = Dir.pwd
-#     input_filenames = ['results.html']
-#
-#     zipfile_name = "#{Date.today}_results.zip"
-#
-#     Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-#       input_filenames.each do |filename|
-#         zipfile.add(filename, folder + '/' + filename)
-#       end
-#     end
-#     FileUtils.mv(zipfile_name,"resultsarchive/#{zipfile_name}")
-#   end
-# end
 
 
 
@@ -212,42 +194,6 @@ at_exit do
     FileUtils.mv(zipfile_name, "#{test_result}/#{zipfile_name}")
   end
 end
-
-
-
-# at_exit do
-#   ENV['ARCHIVE_RESULTS'] = 'no' if ENV['ARCHIVE_RESULTS'].nil?
-#   if ENV['ARCHIVE_RESULTS']=="yes"
-#     # Dir.mkdir("resultsarchive") if Dir["resultsarchive"].empty?
-#     test_result="TEST_RESULT"
-#     Dir.mkdir(test_result) if Dir[test_result].empty? # ---Made changes----------------------------
-#     folder = Dir.pwd
-#     input_filenames = ['results.html']
-#     time = Time.now.strftime("%m-%d-%Y")
-#     time_min = Time.now.strftime("%I.%M_%p")
-#     # zipfile_name = "Date_#{time}_Time_#{time_min}_Results.zip"
-#     zipfile_name = "#{time}_#{time_min}_Results.zip"
-#     Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-#       input_filenames.each do |filename|
-#         zipfile.add(filename, folder + '/' + filename)
-#         if ENV['Email_Test_Results']=="yes"
-#           outlook = WIN32OLE.new('Outlook.Application')
-#           message = outlook.CreateItem(0)
-#           message.Subject = " TEST EXECUTION RESULTS [ #{time}_#{time_min} ]"
-#           message.Body = " Please find the attachment for ***  Automated Test Execution Results  *** "
-#           #message.Recipients.Add 'moklasur.rahman@uscis.dhs.gov'
-#           message.Recipients.Add 'sheikh.rahman@capgemini-gs.com'
-#           message.Attachments.Add(folder + '/' + filename)
-#           #Want to save as a draft?
-#           message.Save
-#           #Want to send instead?
-#           message.Send
-#         end
-#       end
-#     end
-#     FileUtils.mv(zipfile_name, "#{test_result}/#{zipfile_name}")
-#   end
-# end
 
 
 
